32c2652aa069d7824cae4fac590d317d56e44b2a
CultistMeeple <mmangulis@gmail.com>
2022-08-11 14:17:03 +0300

    First scenes with ability to switch between them
    * Created 5 scenes in Scene Builder: default (aka Home), addEntry, addBrewery, addShop, cart (aka Shopping cart);
    * Added functionality for switching between these scenes:
    cartButton switches to cart, homeButton switches to default, addButton switches to addEntry (which by default is addBrewery scene);
    * If addButton is pressed, it takes to a scene containing form for creating Beer objects. If addButton is pressed, also 3 radio buttons show up: Beer, Brewery, Shop. This allows to switch between these 3 different forms.
    * submitButton currently doesn't do anything.
    * The big shopping cart button (which takes to cart scene) has a cartIcon on it.
    * Added title label 'CrafTapp' and an icon of beer mug.
    * Added searchBar and searchButton, which currently only changes the title label (for testing purposes);
    * Added treeView in a split pane. It shows on most scenes (except for cart);
    * treeView is currently only there for testing, will be populated later;

---
+++ src/main/java/com/example/craftapp/DefaultController.java
@@ -0,0 +1,192 @@
+package com.example.craftapp;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeView;
+
+
+public class DefaultController implements Initializable {
+
+    @FXML
+    private Button addButton;
+    @FXML
+    private TextField beerBrewery;
+    @FXML
+    private ImageView beerMug;
+    @FXML
+    private TextField beerName;
+    @FXML
+    private TextField beerType;
+    @FXML
+    private Button homeButton;
+    @FXML
+    private TextField searchBar;
+    @FXML
+    private TextField breweryAdress;
+    @FXML
+    private TextField breweryWebsite;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private RadioButton radioBeer;
+    @FXML
+    private RadioButton radioBrewery;
+    @FXML
+    private RadioButton radioShop;
+
+    @FXML
+    private TextField shopAdress;
+
+    @FXML
+    private TextField shopCity;
+
+    @FXML
+    private TextField shopCountry;
+
+    @FXML
+    private TextField shopName;
+
+    @FXML
+    private TextField shopWebsite;
+    @FXML
+    private ImageView cartIcon;
+    @FXML
+    private Button cartButton;
+    @FXML
+    private Label title;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private TreeView<String> treeView;
+    @FXML
+    void selectItem(MouseEvent event) {
+
+        TreeItem<String>item = treeView.getSelectionModel().getSelectedItem();
+
+        if (item != null) {
+            System.out.println(item.getValue());
+        }
+    }
+    @FXML
+    public void switchToHome (ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("default.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 335,600);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void switchToAddBeer(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("addEntry.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 335,600);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void switchToAddBrewery(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("addBrewery.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 335,600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToAddShop(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("addShop.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 335,600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void switchToCart(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("cart.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 335,600);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        TreeItem<String> treeRoot = new TreeItem<>("Latvia");
+        TreeItem <String> branch1 = new TreeItem<>("Breweries");
+        TreeItem <String> branch2 = new TreeItem<>("Beers");
+
+        if (treeView != null) {
+            treeView.setRoot(treeRoot);
+            treeRoot.getChildren().add(branch1);
+            branch1.getChildren().add(branch2);
+        }
+
+        searchBar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                String text = searchBar.getText();
+                title.setText(text);
+            }
+        });
+        searchButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                title.setText("Button");
+            }
+        });
+
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                try {
+
+                    switchToAddBeer(event);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        homeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+
+                    switchToHome(event);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+    }
+
+}
